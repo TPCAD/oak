f5f193a7a23180d2a9c8ef6a8144ecd10c13c04b
TPCAD <1814083420@qq.com>
2024-04-19 16:27:46 +0800

    feat: memory detection
    Detecting memory with bios interruption 0x15

--- src/loader.asm
+++ src/loader.asm
@@ -2,10 +2,54 @@
 
 dw 0x55aa
 
-xchg bx, bx
-
 mov si, loading
 call print
+
+xchg bx, bx
+
+call detect_memory
+
+jmp $
+
+detect_memory:
+	xor ebx, ebx
+
+	mov ax, 0
+	mov es, ax
+
+	mov edi, ards_buffer
+	mov edx, 0x534d4150
+
+.next:
+	mov eax, 0xe820
+	mov ecx, 20
+	int 0x15
+
+	jc error
+
+	add di, cx
+	inc word [ards_count]
+
+	cmp ebx, 0
+	jnz .next
+
+	mov si, detecting
+	call print
+
+	ret
+	
+; 	mov cx, [ards_count]
+; 	mov si, 0
+;
+; .show:
+; 	mov eax, [ards_buffer + si]
+; 	mov ebx, [ards_buffer + si + 8]
+; 	mov edx, [ards_buffer + si + 16]
+; 	add si, 20
+; 	
+; 	xchg bx, bx
+; 	loop .show
+
 
 print:
 	mov ah, 0x0e
@@ -19,5 +63,20 @@ print:
 .done:
 	ret
 
+error:
+	mov si, .msg
+	call print
+	hlt
+	jmp $
+.msg:
+	db "Loading error...", 13, 10, 0
+
 loading:
 	db "Loading Oak...", 10 ,13, 0
+	
+detecting:
+	db "Detecting memory success...", 10 ,13, 0
+
+ards_count:
+	dw 0
+ards_buffer:
