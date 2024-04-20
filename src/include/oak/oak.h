61677b9a43216b5a9ee7dba9cad821cb7a54f980
TPCAD <1814083420@qq.com>
2024-04-20 12:42:27 +0800

    feat: C code

---
+++ src/include/oak/oak.h
@@ -0,0 +1,8 @@
+#ifndef DEBUG
+#define OAK_H
+
+#define OAK_MAGIC 20240419
+
+void kernel_init();
+
+#endif // !DEBUG
---
+++ src/kernel/main.c
@@ -0,0 +1,10 @@
+#include <oak/oak.h>
+
+char *message = "Hello Oak!";
+
+void kernel_init() {
+  char *video = (char *)0xb8000;
+  for (int i = 0; i < sizeof(message); i++) {
+    video[i * 2] = message[i];
+  }
+}
