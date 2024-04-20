aed4d094646e683c6411a19ed4be7f4cde35b992
TPCAD <1814083420@qq.com>
2024-04-20 09:05:09 +0800

    feat: enter protected mode
    - a null segment, a data segment, a code segment for GDT
    - turn on A20 line
    - enter protected mode

--- src/loader.asm
+++ src/loader.asm
@@ -5,11 +5,9 @@ dw 0x55aa
 mov si, loading
 call print
 
-xchg bx, bx
-
 call detect_memory
 
-jmp $
+jmp 0:pre_protected_mode
 
 detect_memory:
 	xor ebx, ebx
@@ -38,19 +36,6 @@ detect_memory:
 
 	ret
 	
-; 	mov cx, [ards_count]
-; 	mov si, 0
-;
-; .show:
-; 	mov eax, [ards_buffer + si]
-; 	mov ebx, [ards_buffer + si + 8]
-; 	mov edx, [ards_buffer + si + 16]
-; 	add si, 20
-; 	
-; 	xchg bx, bx
-; 	loop .show
-
-
 print:
 	mov ah, 0x0e
 .next:
@@ -71,11 +56,106 @@ error:
 .msg:
 	db "Loading error...", 13, 10, 0
 
+pre_protected_mode:
+
+	; disable interruption
+	cli
+
+	; turn on A20 line
+	in al, 0x92
+	or al, 0b10
+	out 0x92, al
+
+	; load GDT
+	lgdt [gdt_ptr]
+
+	; launch protected mode
+	mov eax, cr0
+	or al, 1
+	mov cr0, eax
+
+	; a far jump after launch immediately
+	jmp dword code_selector:protect_mode
+
+[bits 32]
+protect_mode:
+
+	; initialize segment register
+	mov ax, data_selector
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+
+	mov esp, 0x10000
+
+	mov byte [0xb8000], 'P'
+
+jmp $
+
 loading:
 	db "Loading Oak...", 10 ,13, 0
 	
 detecting:
 	db "Detecting memory success...", 10 ,13, 0
+
+; GDT related
+
+; top 3 bits are 0
+code_selector equ (1 << 3)
+data_selector equ (2 << 3)
+
+memory_base equ 0
+; 0xfffff
+memory_limit equ ((1024 * 1024 * 1024 * 4) / (1024 * 4)) - 1
+
+gdt_ptr:
+	dw (gdt_end - gdt_base) - 1
+	dd gdt_base
+gdt_base:
+	dd 0,0 ; first segment descriptor must be all 0
+gdt_code:
+	dw memory_limit & 0xffff ; limit 0~15 bits
+	dw memory_base & 0xffff ; base 0~15 bits
+	db (memory_base >> 16) & 0xff ; base 16~24 bits
+	; access type.
+	; P: 1, on memory
+	; DPL: 00, ring 0
+	; S: 1, code or data segment
+	; X: 1, code segment
+	; C: 0, no conforming
+	; R: 1, readable
+	; A: 0, no accessed
+	db 0b1_00_1_1010
+	; G: 1, 4K granularity
+	; DB: 1, 32 bits
+	; L: 0, not long-mode
+	; AV: 0
+	; limit 16~19 bits only has 4 bits, so it must be placed with flags
+	db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf
+	db (memory_base >> 24) & 0xff
+gdt_data:
+	dw memory_limit & 0xffff ; limit 0~15 bits
+	dw memory_base & 0xffff ; base 0~15 bits
+	db (memory_base >> 16) & 0xff ; base 16~24 bits
+	; access type.
+	; P: 1, on memory
+	; DPL: 00, ring 0
+	; S: 1, code or data segment
+	; X: 0, data segment
+	; D: 0, direction, grown up
+	; W: 1, writable
+	; A: 0, no accessed
+	db 0b1_00_1_0010
+	; G: 1, 4K granularity
+	; DB: 1, 32 bits
+	; L: 0, not long-mode
+	; AV: 0
+	; limit 16~19 bits only has 4 bits, so it must be placed with flags
+	db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf
+	db (memory_base >> 24) & 0xff
+gdt_end:
 
 ards_count:
 	dw 0
