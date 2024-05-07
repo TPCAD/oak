9f91f9abc5a3c317b655d5a98f9767702c73599c
TPCAD <1814083420@qq.com>
2024-05-07 16:06:13 +0800

    feat: assert and panic

---
+++ src/kernel/assert.c
@@ -0,0 +1,39 @@
+#include <oak/assert.h>
+#include <oak/printk.h>
+#include <oak/stdarg.h>
+#include <oak/stdio.h>
+#include <oak/types.h>
+
+static char buf[1024];
+
+static void spin(char *name) {
+    printk("spinning in %s ...\n", name);
+    while (true)
+        ;
+}
+
+void assert_failure(char *exp, char *file, char *base, int line) {
+    printk("\n--> assert(%s) failed!"
+           "\n--> file: %s"
+           "\n--> base: %s"
+           "\n--> line: %d\n",
+           exp, file, base, line);
+
+    spin("assert_failure()");
+
+    // error if execute this
+    asm volatile("ud2");
+}
+
+void panic(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    int i = vsprintf(buf, fmt, args);
+    va_end(args);
+
+    printk("panic!\n--> %s\n", buf);
+    spin("panic()");
+
+    // error if execute this
+    asm volatile("ud2");
+}
