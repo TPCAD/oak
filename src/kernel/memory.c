cbdd4872ceb973d9cb61a2dfe2979147542883a6
TPCAD <1814083420@qq.com>
2024-05-18 19:38:37 +0800

    feat: kernel virtual memory manage

--- src/kernel/memory.c
+++ src/kernel/memory.c
@@ -1,10 +1,11 @@
-#include "oak/assert.h"
-#include "oak/debug.h"
-#include "oak/oak.h"
-#include "oak/stdlib.h"
-#include "oak/types.h"
+#include <oak/assert.h>
+#include <oak/bitmap.h>
+#include <oak/debug.h>
 #include <oak/memory.h>
+#include <oak/oak.h>
+#include <oak/stdlib.h>
 #include <oak/string.h>
+#include <oak/types.h>
 
 #define ZONE_VALID 1    // valid area of ards
 #define ZONE_RESERVED 2 // invalid area of ards
@@ -21,10 +22,18 @@ static u32 KERNEL_PAGE_TABLE[] = {0x2000, 0x3000};
 
 #define KERNEL_MEMORY_SIZE (0x100000 * sizeof(KERNEL_PAGE_TABLE))
 
+#define KERNEL_MAP_BITS 0x4000
+
+/* A struct to hold ards
+ *
+ * @FIELD base Base address of memory block
+ * @FIELD size Size of memory block
+ * @FIELD type Type of memory block
+ */
 typedef struct ards_t {
-    u64 base; // base address of memory
-    u64 size; // size of memory
-    u32 type; // type of memory
+    u64 base;
+    u64 size;
+    u32 type;
 } _packed ards_t;
 
 static u32 memory_base = 0;
@@ -34,16 +43,24 @@ static u32 free_pages = 0;
 
 #define used_pages (total_pages - free_pages)
 
+/* Check memory status
+ *
+ * This function check the result of `detecting_memory` in loader.asm and store
+ * the value about memory status in four static variable
+ *
+ * @param magic Magic number to compatible with grub
+ * @param addr Address of ards_count
+ */
 void memory_init(u32 magic, u32 addr) {
     u32 count;
     ards_t *ptr;
 
     if (magic == OAK_MAGIC) {
         count = *(u32 *)addr;
-        ptr = (ards_t *)(addr + 4);
+        ptr = (ards_t *)(addr + 4); // points to ards_buffer
+
+        // find out the largest memory block
         for (size_t i = 0; i < count; i++, ptr++) {
-            DEBUGK("Memory base 0x%p size 0x%p type %d\n", (u32)ptr->base,
-                   (u32)ptr->size, (u32)ptr->type);
             if (ptr->type == ZONE_VALID && ptr->size > memory_size) {
                 memory_base = (u32)ptr->base;
                 memory_size = (u32)ptr->size;
@@ -53,18 +70,11 @@ void memory_init(u32 magic, u32 addr) {
         panic("Memory init magic unknown 0x%p\n", magic);
     }
 
-    DEBUGK("ARDS count %d\n", count);
-    DEBUGK("Memory base 0x%p\n", (u32)memory_base);
-    DEBUGK("Memory size 0x%p\n", (u32)memory_size);
-
     assert(memory_base == MEMORY_BASE);
     assert((memory_size & 0xfff) == 0);
 
     total_pages = IDX(memory_size) + IDX(MEMORY_BASE);
     free_pages = IDX(memory_size);
-
-    DEBUGK("Total pages %d\n", total_pages);
-    DEBUGK("Free pages %d\n", free_pages);
 
     if (memory_size < KERNEL_MEMORY_SIZE) {
         panic("System memory is too small, at least %dM needed\n",
@@ -74,15 +84,16 @@ void memory_init(u32 magic, u32 addr) {
 
 static u32 start_page = 0;   // start location of available physical memory
 static u8 *memory_map;       // physical memory array
-static u32 memory_map_pages; // pages
+static u32 memory_map_pages; // pages amount for managing
+
+bitmap_t kernel_map;
 
 void memory_map_init() {
-    // pages amount to manage physical memory
     memory_map_pages = div_round_up(total_pages, PAGE_SIZE);
-    DEBUGK("Memory map pages count: %d\n", memory_map_pages);
 
-    // use start `memory_map_pages` pages to manage physical memory
+    // place the array in memory_base(0x100000)
     memory_map = (u8 *)memory_base;
+    // size of the array
     memset((void *)memory_map, 0, memory_map_pages * PAGE_SIZE);
 
     free_pages -= memory_map_pages;
@@ -93,16 +104,26 @@ void memory_map_init() {
         memory_map[i] = 1;
     }
 
-    DEBUGK("Total pages %d, free pages %d\n", total_pages, free_pages);
+    // DEBUGK("Total pages %d, free pages %d\n", total_pages, free_pages);
+
+    // manage kernel virtual memory with bitmap
+    u32 length = IDX(KERNEL_MEMORY_SIZE) / 8;
+    bitmap_init(&kernel_map, (u8 *)KERNEL_MAP_BITS, length, IDX(MEMORY_BASE));
+    // pages for memory_map
+    bitmap_scan(&kernel_map, memory_map_pages);
 }
 
+/* Allocate one physical page
+ *
+ * @return The page address
+ */
 static u32 alloc_page() {
     for (size_t i = start_page; i < total_pages; i++) {
         if (!memory_map[i]) {
             memory_map[i] = 1;
             free_pages--;
             u32 page = ((u32)i) << 12;
-            DEBUGK("Allocate page 0x%p\n", page);
+            // DEBUGK("Allocate page 0x%p\n", page);
             return page;
         }
     }
@@ -110,6 +131,10 @@ static u32 alloc_page() {
     return 0; // not necessary
 }
 
+/* Free one physical page
+ *
+ * @param addr Address of page to free
+ */
 static void free_page(u32 addr) {
     // must be start location of a page
     ASSERT_PAGE(addr);
@@ -128,21 +153,29 @@ static void free_page(u32 addr) {
     }
 
     assert(free_pages > 0 && free_pages < total_pages);
-    DEBUGK("Free page 0x%p\n", addr);
+    // DEBUGK("Free page 0x%p\n", addr);
 }
 
+/* Get the value of cr3
+ */
 u32 get_cr3() {
     // return value in eax
     asm volatile("movl %cr3, %eax\n");
 }
 
-// set cr3, pde is the address of page index
+/* Set the value of cr3
+ *
+ * @param pde The address of page directory
+ */
 void set_cr3(u32 pde) {
     ASSERT_PAGE(pde);
     asm volatile("movl %%eax, %%cr3\n" ::"a"(pde));
 }
 
-// set the highest bit of cr0 to 1 to enable page
+/* Enable paging
+ *
+ * Set the highest bit of cr0 to 1 to enable paging
+ */
 static _inline void enable_page() {
     // 0b1000_0000_0000_0000_0000_0000_0000_0000
     // 0x80000000
@@ -151,7 +184,11 @@ static _inline void enable_page() {
                  "movl %eax, %cr0\n");
 }
 
-// init page table entry or page directory entry
+/* Initialize page table entry of page directory entry
+ *
+ * @param entry Address of entry
+ * @param index Address of page table or page directory
+ */
 static void entry_init(page_entry_t *entry, u32 index) {
     *(u32 *)entry = 0;
     entry->present = 1;
@@ -160,13 +197,19 @@ static void entry_init(page_entry_t *entry, u32 index) {
     entry->index = index;
 }
 
+/* Mapping kernel memory and enable paging
+ *
+ * Place page directory which is 4K bytes in size in 0x1000.Page directory has
+ * 3 page table in index 0, 1 and 1023.The top 2 map the virtual 8M to physical
+ * 8M(ignore first 4K). The last one in index 1023 points to the directory
+ * itself therefore we can modify the page directory and page table through
+ * this entry.
+ */
 void mapping_init() {
-    // page directory entry
     page_entry_t *pde = (page_entry_t *)KERNEL_PAGE_DIR;
     memset(pde, 0, PAGE_SIZE);
 
     idx_t index = 0;
-
     for (idx_t didx = 0; didx < (sizeof(KERNEL_PAGE_TABLE) / 4); didx++) {
         page_entry_t *pte = (page_entry_t *)KERNEL_PAGE_TABLE[didx];
         memset(pte, 0, PAGE_SIZE);
@@ -189,55 +232,109 @@ void mapping_init() {
     page_entry_t *entry = &pde[1023];
     entry_init(entry, IDX(KERNEL_PAGE_DIR));
 
+    // enable paging
     set_cr3((u32)pde);
-
     enable_page();
 }
 
+/* Get page directory
+ *
+ * @return Address of page directory
+ */
 static page_entry_t *get_pde() { return (page_entry_t *)(0xfffff000); }
 
+/* Get page table entry
+ *
+ * @param vaddr Address of page?
+ *
+ * @return Address of page table
+ */
 static page_entry_t *get_pte(u32 vaddr) {
     return (page_entry_t *)(0xffc00000 | (DIDX(vaddr) << 12));
 }
 
+/* Flush table
+ *
+ * @param vaddr Virtual address of page table
+ */
 static void flush_tlb(u32 vaddr) {
     asm volatile("invlpg (%0)" ::"r"(vaddr) : "memory");
 }
 
+/* Get continuous page
+ *
+ * @param map Bitmap
+ * @param count Amount of pages to allcate
+ *
+ * @return Address of page allocated
+ */
+static u32 scan_page(bitmap_t *map, u32 count) {
+    assert(count > 0);
+    int index = bitmap_scan(map, count);
+
+    if (index == EOF) {
+        panic("Not enough pages!\n");
+    }
+
+    u32 addr = PAGE(index);
+    DEBUGK("Scan page 0x%p count %d\n", addr, count);
+    return addr;
+}
+
+/* Reset page
+ *
+ * @param map Bitmap
+ * @param addr Address of page
+ * @param count Amount of page
+ */
+static void reset_page(bitmap_t *map, u32 addr, u32 count) {
+    ASSERT_PAGE(addr);
+    assert(count > 0);
+
+    u32 index = IDX(addr);
+
+    for (size_t i = 0; i < count; i++) {
+        assert(bitmap_is_set(map, index + i));
+        bitmap_set(map, index + i, false);
+    }
+}
+
+/* Allocate continuous kernel page
+ *
+ * @param count Amount of pages
+ *
+ * @return Address of start page
+ */
+u32 alloc_kpage(u32 count) {
+    assert(count > 0);
+    u32 vaddr = scan_page(&kernel_map, count);
+    DEBUGK("allocate kernel page 0x%p count %d\n", vaddr, count);
+    return vaddr;
+}
+
+/* Free continuous kernel page
+ *
+ * @param vaddr Address of page to free
+ * @param count Amount of pages
+ *
+ * @return Address of start page
+ */
+void free_kpage(u32 vaddr, u32 count) {
+    ASSERT_PAGE(vaddr);
+    assert(count > 0);
+    reset_page(&kernel_map, vaddr, count);
+    DEBUGK("free kernel page 0x%p count %d\n", vaddr, count);
+}
+
 void memory_test() {
-    BMB;
+    u32 *pages = (u32 *)(0x200000);
+    u32 count = 0x6ff;
+    for (size_t i = 0; i < count; i++) {
+        pages[i] = alloc_kpage(1);
+        DEBUGK("0x%x\n", i);
+    }
 
-    // 将 20 M 0x1400000 内存映射到 64M 0x4000000 的位置
-
-    // 我们还需要一个页表，0x900000
-
-    u32 vaddr = 0x4000000; // 线性地址几乎可以是任意的
-    u32 paddr = 0x1400000; // 物理地址必须要确定存在
-    u32 table = 0x900000;  // 页表也必须是物理地址
-
-    page_entry_t *pde = get_pde();
-
-    page_entry_t *dentry = &pde[DIDX(vaddr)];
-    entry_init(dentry, IDX(table));
-
-    page_entry_t *pte = get_pte(vaddr);
-    page_entry_t *tentry = &pte[TIDX(vaddr)];
-
-    entry_init(tentry, IDX(paddr));
-
-    BMB;
-
-    char *ptr = (char *)(0x4000000);
-    ptr[0] = 'a';
-
-    BMB;
-
-    entry_init(tentry, IDX(0x1500000));
-    flush_tlb(vaddr);
-
-    BMB;
-
-    ptr[2] = 'b';
-
-    BMB;
+    for (size_t i = 0; i < count; i++) {
+        free_kpage(pages[i], 1);
+    }
 }
