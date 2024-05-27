fdb6fcfaa68637cc3efe918ee227a27a92897354
TPCAD <1814083420@qq.com>
2024-05-27 19:57:19 +0800

    chore: kernel init

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,14 +1,27 @@
-#include "oak/printk.h"
-#include <oak/console.h>
-#include <oak/io.h>
-#include <oak/oak.h>
-#include <oak/printk.h>
-#include <oak/stdarg.h>
-#include <oak/stdio.h>
 #include <oak/types.h>
+extern void interrupt_init();
+extern void clock_init();
+extern void time_init();
+extern void rtc_init();
+extern void memory_map_init();
+extern void mapping_init();
+extern void hang();
+extern void task_init();
+extern void set_interrupt_state(bool state);
+extern void syscall_init();
 
 void kernel_init() {
-    console_init();
-    printk("%s", "hello");
+    memory_map_init();
+    mapping_init();
+
+    interrupt_init();
+    clock_init();
+
+    // time_init();
+    // rtc_init();
+    task_init();
+    syscall_init();
+    set_interrupt_state(true);
+
     return;
 }
