1ef0bbe05701be5454385500b03553a474ca3147
TPCAD <1814083420@qq.com>
2024-05-27 19:32:59 +0800

    feat: init before kernel

--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -1,8 +1,19 @@
 [bits 32]
 
+extern console_init
+extern gdt_init
+extern memory_init
 extern kernel_init
+
 global _start
 _start:
-	call kernel_init
+	push ebx
+	push eax
 	
+	call console_init
+	call gdt_init
+	call memory_init
+	
+	call kernel_init
+
 	jmp $
