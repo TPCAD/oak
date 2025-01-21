0100630d978f190fb0cf6475fd7a037ac47b529c
TPCAD <clockcuckoo@outlook.com>
2025-01-21 09:46:24 +0800

    refactor: remove old files

--- src/include/oak/oak.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEBUG
-#define OAK_H
-
-#define OAK_MAGIC 0x20240419
-
-void kernel_init();
-
-#endif // !DEBUG
--- src/include/oak/printk.h
+++
@@ -1,6 +0,0 @@
-#ifndef OAK_PRINTK_H
-#define OAK_PRINTK_H
-
-int printk(const char *fmt, ...);
-
-#endif // !OAK_PRINTK_H
