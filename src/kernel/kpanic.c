1c7ca6952120f60d9c86f5c05b4917457a491c63
TPCAD <1814083420@qq.com>
2025-03-10 16:16:50 +0800

    fix(lib): remove unused variable

--- src/kernel/kpanic.c
+++ src/kernel/kpanic.c
@@ -8,7 +8,7 @@ static char buf[1024];
 void kpanic(const char *fmt, ...) {
     va_list vlist;
     va_start(vlist, fmt);
-    int i = vsprintf(buf, fmt, vlist);
+    vsprintf(buf, fmt, vlist);
     va_end(vlist);
 
     kprintf("kernel panic\n%s \n", buf);
