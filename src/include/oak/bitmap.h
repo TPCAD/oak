4962f9b92ba7a22be17ae821c923f8e9a64098df
TPCAD <1814083420@qq.com>
2025-04-03 16:50:52 +0800

    feat(lib): bitmap lib

---
+++ src/include/oak/bitmap.h
@@ -0,0 +1,19 @@
+#ifndef OAK_BITMAP_H
+#define OAK_BITMAP_H
+
+#include <oak/types.h>
+
+typedef struct bitmap_t {
+    u8 *buf;
+    u32 size;
+    u32 offset;
+} bitmap_t;
+
+void bitmap_create(bitmap_t *map, u8 *buf, u32 size, u32 offset);
+void bitmap_init(bitmap_t *map, u8 *buf, u32 size, u32 offset);
+bool bitmap_is_set(bitmap_t *map, u32 idx);
+void bitmap_set_bit(bitmap_t *map, u32 idx, bool value);
+void bitmap_set_bits(bitmap_t *map, u32 idx, u32 count, bool value);
+u32 bitmap_find_bits(bitmap_t *map, u32 count);
+
+#endif // !OAK_BITMAP_H
