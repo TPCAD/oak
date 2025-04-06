3703b9fb112d8d158de907cebcdb96f9ca4b05bc
TPCAD <1814083420@qq.com>
2025-04-06 15:10:13 +0800

    feat(kernel): mutex

---
+++ src/kernel/mutex.c
@@ -0,0 +1,65 @@
+#include "oak/debug/kassert.h"
+#include "oak/list.h"
+#include "oak/oak.h"
+#include "oak/task.h"
+#include <oak/cpu.h>
+#include <oak/mutex.h>
+
+/**
+ *  @brief  初始化互斥量
+ *  @param  mutex  互斥量
+ */
+void mutex_init(mutex_t *mutex) {
+    mutex->value = false;
+    list_init(&mutex->waiters);
+}
+
+/**
+ *  @brief  尝试持有互斥量
+ *  @param  mutex  互斥量
+ *
+ *  尝试失败会进入阻塞状态，直至互斥量被释放
+ */
+void mutex_lock(mutex_t *mutex) {
+    bool intr = cpu_diable_intr();
+    task_t *curr_task = task_current_running();
+
+    // 尝试持有互斥量，若互斥量已被持有则进入阻塞状态
+    while (mutex->value == true) {
+        task_block(curr_task, &mutex->waiters, TASK_BLOCKED);
+    }
+
+    kassert(mutex->value == false);
+
+    // 持有互斥量
+    mutex->value++;
+    kassert(mutex->value == true);
+
+    // 恢复中断状态
+    cpu_set_intr_state(intr);
+}
+
+/**
+ *  @brief  释放互斥量
+ *  @param  mutex  互斥量
+ */
+void mutex_unlock(mutex_t *mutex) {
+    bool intr = cpu_diable_intr();
+
+    kassert(mutex->value == true);
+
+    mutex->value--;
+    kassert(mutex->value == false);
+
+    // 唤醒正在等待互斥量的任务
+    if (!list_is_empty(&mutex->waiters)) {
+        task_t *task = element_entry(task_t, node, mutex->waiters.tail.prev);
+        kassert(task->magic == OAK_MAGIC);
+        task_unblock(task);
+
+        // 主动让出 CPU，防止其他任务饿死
+        task_yield();
+    }
+
+    cpu_set_intr_state(intr);
+}
