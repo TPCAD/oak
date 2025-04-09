23e598bdc7434dc9a5263f91c2cc95580b88c177
TPCAD <1814083420@qq.com>
2025-04-09 15:35:28 +0800

    feat(lib): macro ROUNDUP

--- src/include/oak/stdlib.h
+++ src/include/oak/stdlib.h
@@ -1,6 +1,9 @@
 #ifndef OAK_STDLIB_H
 #define OAK_STDLIB_H
 
+/* 将 v 向上取整至 k 的倍数，k 必须为 2 的幂 */
+#define ROUNDUP(v, k) (((v) + (k) - 1) & ~((k) - 1))
+
 void itoa(int input, char *buffer);
 
 #endif // !OAK_STDLIB_H
