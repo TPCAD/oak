6b8b71838d9c40e6a87a048b4a6b76968aad8196
TPCAD <1814083420@qq.com>
2025-04-15 12:38:33 +0800

    fix(fs): mount not work
    return mounted device root inode if directory mounted device instead
    itself

--- src/kernel/fs/minix/inode.c
+++ src/kernel/fs/minix/inode.c
@@ -70,6 +70,11 @@ static inode_t *search_inode(u32 dev, u32 nr) {
     return NULL;
 }
 
+/**
+ *  @brief  检查 inode 是否有挂载目录
+ *  @param  inode
+ *  @return  若有挂载目录则返回挂载的超级块
+ */
 static inode_t *fit_inode(inode_t *inode) {
     if (!inode->mount)
         return inode;
@@ -112,7 +117,7 @@ inode_t *inode_search(u32 dev, u32 nr) {
     if (inode) {
         inode->count++;
         inode->atime = time();
-        return inode;
+        return fit_inode(inode);
     }
 
     super_block_t *sb = search_super_block(dev);
@@ -135,7 +140,7 @@ inode_t *inode_search(u32 dev, u32 nr) {
     inode->ctime = inode->inode->mtime;
     inode->atime = time();
 
-    return fit_inode(inode);
+    return inode;
 }
 
 /**
